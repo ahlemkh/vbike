e5f55f2b44cfce5f7a336202ca8864608c389eb6
ahlemkh <112702108+ahlemkh@users.noreply.github.com>
2023-02-21 13:00:15 +0100

    update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
-<?php try {$connection=new PDO('mysql:host=localhost;db_name=bike_rental;charset=utf8','root','');}
-        catch( Exeption $e)
-        {
-            die('Erreur : '.$e->getMessage());
-        }
-        /*,
-            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]*/
-        ?>
+<?php
+  $dsn = "mysql:host={$_ENV["HOST"]};dbname={$_ENV["DATABASE"]}";
+  $options = array(
+    PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
+  );
+
+  $pdo = new PDO($dsn, $_ENV["USERNAME"], $_ENV["PASSWORD"], $options);
+?>

37f47845b53c5cd9adca3acf8a203f3e1b204007
ahlemkh <112702108+ahlemkh@users.noreply.github.com>
2023-02-21 19:19:58 +0100

    f

--- connect.php
+++ connect.php
@@ -3,5 +3,7 @@ $dsn = "mysql:host={$_ENV['HOST']};dbname={$_ENV['DATABASE']}";
   $options = array(
     PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/ca-bundle.pem",
   );
+echo $_ENV['HOST'];
 
   $connection = new PDO($dsn, $_ENV["USERNAME"], $_ENV["PASSWORD"], $options);
+

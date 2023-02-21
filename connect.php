67f685a70a5535fbf84171933f4c91643635689d
ahlemkh <112702108+ahlemkh@users.noreply.github.com>
2023-02-21 13:45:03 +0100

    c

--- connect.php
+++ connect.php
@@ -1,14 +1,10 @@
 <?php
- 
-  $options = array(
-    PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
-  );
-
- //$connection = new PDO($dsn, $_ENV["USERNAME"], $_ENV["PASSWORD"], $options);
-
 $HOST='eu-central.connect.psdb.cloud';
 $USERNAME='jrwdj1lvec0b6nmggu0f';
 $PASSWORD='pscale_pw_xcaQa77LUySApLCiNkRJM1XycOE6KtejqAPHmP9kBJ4';
 $DATABASE='bike_rental';
-$connection=new PDO('mysql:host='.$HOST.';db_name='.$DATABASE.';charset=utf8',$USERNAME,$PASSWORD);
-?>
+$options = array(
+    PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
+  );
+$connection=new PDO('mysql:host='.$HOST.';db_name='.$DATABASE,
+$USERNAME,$PASSWORD,$options);
